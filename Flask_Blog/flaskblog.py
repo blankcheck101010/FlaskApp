cc99a66dc884f7392b88132458a6e2a11ecaba6d
blankcheck101010 <saulalvarez86@gmail.com>
2018-06-08 00:15:54 -0700

    first webpage

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,1 +1,21 @@
-import flask
+from flask import Flask
+app = Flask(__name__)  # this is the name of the module
+
+# '/' the root(home) page of website
+# decorators add additional functionality to existing
+# functions
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return "<h1>Home Page</h1>"
+
+
+@app.route("/about")
+def hello():
+    return "<h1>About Page</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
