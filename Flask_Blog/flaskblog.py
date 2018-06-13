7aa983a78cf436492048f6d970fd7ede38a358b6
blankcheck101010 <saulalvarez86@gmail.com>
2018-06-12 22:16:36 -0700

    added templates using snippets to Blog Project

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,5 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)  # this is the name of the module
+
+
+posts = [
+    {
+        'author': 'Saul Alvarez',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Maria Alvarez',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 
 # '/' the root(home) page of website
 # decorators add additional functionality to existing
@@ -9,12 +26,12 @@ app = Flask(__name__)  # this is the name of the module
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def hello():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
