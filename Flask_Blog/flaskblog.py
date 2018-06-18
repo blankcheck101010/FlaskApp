a9e45f8b794ac7516303880d828f67effd5b1aa3
blankcheck101010 <saulalvarez86@gmail.com>
2018-06-17 22:07:33 -0700

    added the logini and register url pages

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)  # this is the name of the module
 
+app.config['SECRET_KEY'] = '1f97f91cfded7cfb92a9fcef1b36db91'
 
 posts = [
     {
@@ -30,8 +32,29 @@ def home():
 
 
 @app.route("/about")
-def hello():
+def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
